4f374d68e9ed9c6864da59664b832c3795a4e5d0
ninabreznik <ninabreznik@gmail.com>
2018-08-21 02:22:16 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -9,34 +9,49 @@ module.exports = displayIntegerInput
 ----------------------- */
 
 function displayIntegerInput({theme: {classes: css}, type}) {
+  var num = bel`<input class=${css.integerValue} min="" max="" value="50" oninput=${(e)=>sliderUpdate(e)} onkeydown=${(e)=>keysUpdating(e)}>`
+  var slider = bel`<input class=${css.integerSlider} type="range" min="" max="" value="50" step="1" oninput=${(e)=>numUpdate(e)}>`
   var type = intOrUint(type)
-  var input = bel`<input class=${css.inputField} onchange=${e=>validateInput({ type, e})} placeholder='123'>`
+
   return bel`
     <div class=${css.integerField}>
-      <div class=${css.minus} onclick=${()=>decrement()}>
-        <i class="${css.icon} fa fa-minus"></i>
-      </div>
-      ${input}
-      <div class=${css.plus} onclick=${()=>increment()}>
-        <i class="${css.icon} fa fa-plus"></i>
-      </div>
+      ${slider}
+      ${num}
     </div>
   `
-  function increment (e) {
-    var value = input.value
-    value ++
-    input.value = value
+  function numUpdate (e) {
+    num.value = e.target.value;
   }
 
-  function decrement (e) {
-    console.log(input.value)
-    var value = input.value
-    value --
-    input.value = value
+  function keysUpdating (e) {
+    var key = e.which
+    var val = parseInt(e.target.value)
+    if (key === 38 && val != slider.max) {
+      slider.value = num.value = val + 1
+    }
+    else if (key === 40 && val != slider.min) {
+      slider.value = num.value = val - 1
+    }
   }
 
-  function intOrUint (type) {
-    return type.search(/\bint/) != -1 ? 'int' : 'uint'
+  function sliderUpdate (e) {
+    if (e.target.value === '') {
+      slider.value = num.value = 0
+    } else {
+      slider.value = e.target.value
+    }
+  }
+
+  function intOrUint (t) {
+    var type = t.search(/\bint/) != -1 ? 'int' : 'uint'
+    if (type === 'int') {
+      slider.min = num.min = -100
+      slider.max = num.max = 100
+    }
+    else if (type === 'uint') {
+      slider.min = num.min = 0
+      slider.max = num.max = 100
+    }
   }
 
 }

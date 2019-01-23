1cf1e68083a29411aca3704ed8bf0b30c5101d6a
ninabreznik <ninabreznik@gmail.com>
2019-01-23 08:33:56 +0100

    Remove flexible range

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ module.exports = displayIntegerInput
 ----------------------- */
 
 function displayIntegerInput({theme: {classes: css}, type, cb}) {
-  var range = getRange(type)
   var min = validator.getRange(type).MIN
   var max = validator.getRange(type).MAX
   var title = `Valid values for type ${type} are from ${min} to ${max}`
-  var num = bel`<input class=${css.integerValue} title=${title} min=${min} max=${max} value="0" oninput=${(e)=>sliderUpdate(e, type)} onkeydown=${(e)=>keysUpdating(e, type)}>`
-  var slider = bel`<input class=${css.integerSlider} type="range" title=${title} min=${min} max=${max} value="0" step=${range} oninput=${(e)=>numUpdate(e, type)}>`
+  var num = bel`<input type="text" class=${css.integerValue} title=${title} min=${min} max=${max} value="0" oninput=${(e)=>sliderUpdate(e, type)} onkeydown=${(e)=>keysUpdating(e, type)}>`
+  var slider = bel`<input class=${css.integerSlider} type="range" title=${title} min=${min} max=${max} value="0" step="1" oninput=${(e)=>numUpdate(e, type)}>`
   return bel`
     <div class=${css.integerField}>
       ${slider}
@@ -24,15 +23,6 @@ function displayIntegerInput({theme: {classes: css}, type, cb}) {
   function numUpdate (e, type) {
     num.value = e.target.value
     validate(e, type)
-  }
-
-  function getRange (type) {
-    var size = type.split('int')[1] // get integer size (8, 16, 256 etc.)
-    if (size < 16) return "1"
-    if (size < 24) return "1000"
-    if (size < 32) return "100000"
-    if (size < 256) return "100000000000"
-    if (size <= 256) return "500000000000000000000000000000000000000000000000000000000000000000000000000"
   }
 
   function validate (e, type) {
